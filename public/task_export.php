8d23e767b6706c788e49871277df7c43cbc56b3d
Virat Tripathi <viratofficial07@gmail.com>
2024-08-29 11:17:04 +0530

    fix: report configuration

--- public/task_export.php
+++ public/task_export.php
@@ -1,11 +1,12 @@
 <?php
 
-$DB_CONNECTION = "mysql";
+
+$DB_CONNECTION = "mysql_docker";
 $DB_HOST = "127.0.0.1";
-$DB_PORT = 3306;
+$DB_PORT = 3307;
 $DB_DATABASE = "mini_jarvis";
 $DB_USERNAME = "root";
-$DB_PASSWORD = "";
+$DB_PASSWORD = "jarvis@123";
 
 $mysqli = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE, $DB_PORT);
 
